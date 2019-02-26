24da48bf437b3c1d80ce98ed0f6a18f93e6abfb6
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-02-26 18:24:44 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
 import './index.css'
 
-class LikeButton extends Component {
-    constructor () {
-        super()
-        this.state = { isLiked: false }
-    }
-
-    handleClickOnLikeButton () {
-        this.setState({
-            isLiked: !this.state.isLiked
-        })
-    }
-
-    render () {
-        return (
-            <button onClick={this.handleClickOnLikeButton.bind(this)}>
-            {this.state.isLiked ? '取消' : '点赞'} 👍
-            </button>
-        )
-    }
-}
-
 ReactDOM.render(
-    <LikeButton />,
+    <CommentApp />,
     document.getElementById('root')
 )
