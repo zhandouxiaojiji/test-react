3ffabd17ce0fb4070bd31f4c716ec95847de7995
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-02-26 19:42:27 +0800

    test

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -3,14 +3,27 @@ import CommentInput from './CommentInput'
 import CommentList from './CommentList'
 
 class CommentApp extends Component {
-  render() {
-    return (
-      <div>
-        <CommentInput />
-        <CommentList />
-      </div>
-    )
-  }
+    constructor() {
+        super()
+        this.state = {
+            comments: []
+        }
+    }
+    onSubmit(comment) {
+        console.log(comment)
+        this.state.comments.push(comment)
+        this.setState({
+            comments: this.state.comments
+        })
+    }
+    render() {
+        return (
+            <div className='wrapper'>
+                <CommentInput onSubmit={this.onSubmit.bind(this)} />
+                <CommentList comments={this.state.comments}/>
+            </div>
+        )
+    }
 }
 
 export default CommentApp
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Comment from './Comment'
 
 class CommentList extends Component {
-  render() {
-    return (
-      <div>CommentList</div>
-    )
-  }
+    static defaultProps = {
+        comments: []
+    }
+    render() {
+        return (
+            <div>{
+                (this.props.comments || this.defaultProps).map((comment, i) => <Comment comment={comment} key={i} />)
+            }</div>
+        )
+    }
 }
 
 export default CommentList
