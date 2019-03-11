93ba92a9811682bbc9075f623b576ff613b2c6f5
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-03-11 16:26:39 +0800

    no router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+class About extends Component {
+  render() {
+    return(<p>cur about</p>);
+  }
+};
+class Inbox extends Component {
+  render() {
+    return(<p>cur inbox</p>);
+  }
+};
+class Home extends Component {
+  render() {
+    return(<p>cur home</p>);
+  }
+};
 
 class App extends Component {
+  componentWillMount() {
+    this.setState({
+      route: window.location.hash.substr(1)
+    })
+  }
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Bamboo~
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    let Child
+    switch (this.state.route) {
+      case '/about': Child = About; break;
+      case '/inbox': Child = Inbox; break;
+      default:      Child = Home;
+    }
+    return(
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><a href="#/about">About</a></li>
+          <li><a href="#/inbox">Inbox</a></li>
+        </ul>
+        <Child/>
       </div>
     );
   }
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CommentApp from './CommentApp'
+import App from './App'
 import './index.css'
 
 ReactDOM.render(
-    <CommentApp />,
+    <App />,
     document.getElementById('root')
 )
